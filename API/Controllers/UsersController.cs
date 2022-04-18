73b7418069eaafaa6b13130c541272892732d74e
bhargavkoya <you@example.com>
2022-04-18 23:34:01 +0530

    AddedUpdate,LoadingIndicators and persistencechangesinAPI

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
 using API.Entities;
 using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -15,10 +17,12 @@ namespace API.Controllers
    [Authorize]
     public class UsersController : BaseApiController
     {
+        private readonly IMapper _mapper;
         
         private readonly IUserRepository _userRepository;
-        public UsersController(IUserRepository userRepository)
+        public UsersController(IUserRepository userRepository,IMapper mapper )
         {
+            _mapper = mapper;
             _userRepository = userRepository;
             
         }
@@ -40,6 +44,23 @@ namespace API.Controllers
             return await _userRepository.GetMemberAsync(username);
 
         }
+
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+
+            _mapper.Map(memberUpdateDto, user);
+
+            _userRepository.Update(user);
+
+            if (await _userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update user");
+        }
+
 
     }
 }
